06ac89ced31725c2f3dc0b9db0d49c6a19223b22
Wolfknightx <rc5340oc@go.minneapolis.edu>
2016-02-18 18:05:17 -0600

    Class project which I'm using to set up my first Java Git

---
+++ src/com/Shawn/Main.java
@@ -0,0 +1,16 @@
+package com.Shawn;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        NewGitPractClass t_Rex = new NewGitPractClass();
+        NewGitPractClass velociraptor = new NewGitPractClass();
+        NewGitPractClass mander = new NewGitPractClass();
+
+        t_Rex.roar(); //this calls the class and the sub function of that class.
+        velociraptor.sup(); //this is another example of calling the class and pulling a new sub function.
+        mander.reh();
+    }
+}
---
+++ src/com/Shawn/NewGitPractClass.java
@@ -0,0 +1,16 @@
+package com.Shawn;
+
+/**
+ * Created by Wolfknightx on 2/18/2016.
+ */
+public class NewGitPractClass {
+    public  void roar(){
+        System.out.println("Grrr!!!!!");
+    }
+    public  void sup(){
+        System.out.println("Sup bro?  Whatz your prob?");
+    }
+    public void reh(){
+        System.out.println("Stupid BIGS!!!");
+    }
+}
